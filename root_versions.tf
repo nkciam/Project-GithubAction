04c866349b3fec9e927257ab2f0c55fa28ea972c
chungnk <daiangclubs@gmail.com>
2023-06-29 18:54:37 +0700

    Update Github Action and Storage Account name

--- root_versions.tf
+++ root_versions.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate"
-    storage_account_name = ""
+    storage_account_name = "tfstate18717"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
